be5359e9723abb94fecdafc2015fd4fd411052ee
Phil Calvin <phil@philcalvin.com>
2019-03-23 14:10:45 -0400

    Ignore apt-get install errors due to ~Debian boog

--- prepare-valgrind.sh
+++ prepare-valgrind.sh
@@ -2,5 +2,6 @@
 set -ex
 
 echo "deb http://deb.debian.org/debian-debug/ unstable-debug main" > /etc/apt/sources.list.d/source.list
-apt-get update && apt-get install -y clang curl sqlite3-dbgsym sqlite3 libsqlite3-dev valgrind
+apt-get update
+apt-get install -y clang curl sqlite3-dbgsym sqlite3 libsqlite3-dev valgrind || echo "Ignoring installation problems due to https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=923942"
 curl https://sh.rustup.rs -sSf | bash -s -- -y
